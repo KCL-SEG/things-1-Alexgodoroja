609bc8d07d604c1d713a3d79975bedc4e13be02e
Alexandru Godoroja <alexandru.godoroja@kcl.ac.uk>
2022-10-22 23:12:42 +0100

    Exercise 3

--- things/models.py
+++ things/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=520)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120)
     quantity = models.IntegerField()
+    if quantity<0 or quantity>100:
+        raise ValidationError('Start date is after end date')
